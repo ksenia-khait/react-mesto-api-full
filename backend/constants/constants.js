5a581d6038c684a6bfc1d87589290c944c00e8b4
ksenia-khait <89581440+ksenia-khait@users.noreply.github.com>
2022-07-18 22:00:02 +0800

    git push

--- backend/constants/constants.js
+++
@@ -1,6 +0,0 @@
-const regExLink = /http(s?):\/\/(www\.)?[0-9a-zA-Z-]+\.[a-zA-Z]+([0-9a-zA-Z-._~:?#[\]@!$&'()*+,;=]+)/;
-const regExEmail = /^\w+([.-]?\w+)*@\w+([.-]?\w+)*(\.\w{2,3})+$/;
-module.exports = {
-  regExLink,
-  regExEmail,
-};

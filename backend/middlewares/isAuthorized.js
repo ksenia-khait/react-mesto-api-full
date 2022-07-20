2b8c4158794cc85230f23364bd236031f667aff1
ksenia-khait <89581440+ksenia-khait@users.noreply.github.com>
2022-07-20 23:46:58 +0800

    Update isAuthorized.js

--- backend/middlewares/isAuthorized.js
+++ backend/middlewares/isAuthorized.js
@@ -1,4 +1,4 @@
-// const { JWT_SECRET } = process.env;
+const { JWT_SECRET } = process.env;
 const jwt = require('jsonwebtoken');
 const UnauthorizedError = require('../errors/unathorizedError');
 
@@ -11,7 +11,7 @@ module.exports.auth = (req, res, next) => {
   let payload;
 
   try {
-    payload = jwt.verify(token, 'very-secret-key');
+    payload = jwt.verify(token, JWT_SECRET);
   } catch (err) {
     throw new UnauthorizedError('Необходимо пройти авторизацию');
   }
